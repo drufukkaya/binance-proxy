d1fa2d7930da8180f122de526340901b39f7c7d0
drufukkaya <drufukkaya091@gmail.com>
2025-08-08 08:36:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/prices', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Proxy server is running on port ${PORT}');
+  console.log(Proxy server is running on port ${PORT});
 });
